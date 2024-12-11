9e5997c19dd370c9ae9e5cbab86f9e6b5633c50b
zyddst <zyddst1314@163.com>
2024-12-11 17:08:48 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  globalData: {
+    userInfo: null
+  }
+}) 
